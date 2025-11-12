c311b6693d6af83781bc05aa5577a856f72705a8
prashant Maurya <prashantmaurya1009@gmail.com>
2025-11-12 21:36:37 +0530

    style change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePage } from "./pages/home";
+import { useEffect } from "react";
 
 /**
  * @typedef {import("react").HTMLProps & import("react").HTMLAttributes} JSXProps
  */
 function App() {
+  useEffect(() => {
+    if (
+      localStorage.getItem("dark") !== "false" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      // setDarkMode(true);
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
